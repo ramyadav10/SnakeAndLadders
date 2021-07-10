c44f6fe1fb7d88fc30dcaecce3446b5a2e241d50
ramyadav10 <ramyadav.now@gmail.com>
2021-07-10 08:03:17 +0530

    UC01-Initialization of player

---
+++ SnakeAndLaderUC1.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class InitializePosition {
+
+static int NUMBER_OF_PLAYER=1;
+static int INITIAL_POSITION=0;
+
+public static void main(String[] args) {
+
+	System.out.println("welcome to snake and ladder");
+
+}
+
+}
